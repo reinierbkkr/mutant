eaacb176cf5e6fa499dc0157efd3a8a664cf93d5
Reinier Bakker <rbakker@sogyo.nl>
2024-03-13 16:59:05 +0100

    bezig met vue basics

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import CountButton from './CountButton.vue';
+
 defineProps<{
   msg: string
 }>()
@@ -7,12 +9,8 @@ defineProps<{
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
-    </h3>
   </div>
+  <CountButton />
 </template>
 
 <style scoped>
