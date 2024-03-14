347d78f06128b576ae88a81f0945049f039145e9
Reinier Bakker <rbakker@sogyo.nl>
2024-03-14 10:47:12 +0100

    GL-3: click button play sample

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,40 +1,13 @@
 <script setup lang="ts">
-import { ref, computed } from 'vue'
-import { fetchMessage } from './api'
-import { storeClickCount } from './api'
-import { fetchClickCount } from './api'
-
-let message = ref("message");
-// let count = ref(0);
-// let clickedMessage = ref("clicked " + count.value + " times")
-
-async function getMessage(){
-  const fetchedMessage = await fetchMessage();
-  message.value = fetchedMessage;
-}
-
-let count = ref(0);
-const clickedMessage = computed(() => `clicked ${count.value} times`);
-
-async function storeClicked(){
-  await storeClickCount(count.value +1);
-  count.value =  await fetchClickCount();}
+import PlayAudioButton from './PlayAudioButton.vue'
+import BETestButtons from './BETestButtons.vue'
 </script>
 
 <template>
-    <button @click="getMessage">get message </button>
-    <div id="message">{{ message }}</div>
-    <button @click="storeClicked">{{ clickedMessage }}</button>
+    <PlayAudioButton />
+    <BETestButtons />
 </template>
 
 <style scoped>
-button {
-  display: block;
-  margin: 2rem auto 2rem;
-}
 
-#message {
-  display: block;
-  margin: 2rem auto 2rem;
-}
 </style>
---
+++ client/src/components/PlayAudioButton.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+    // import { ref, } from 'vue'
+
+    async function playAudio(){
+        const audio = new Audio('/LegoweltSnare001.wav')
+        audio.play()
+    }
+</script>
+
+<template>
+    <button @click="playAudio">play audio</button>
+</template>
+
+<style scoped>
+button {
+  display: block;
+  margin: 2rem auto 2rem;
+}
+</style>
