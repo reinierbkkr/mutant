5859847ff773ff368f594b1fd4cd1a2dbd2e0320
Reinier Bakker <rbakker@sogyo.nl>
2024-03-14 11:48:48 +0100

    GL-3: front end kan sample spelen op loop

--- client/src/components/PlayAudioButton.vue
+++ client/src/components/PlayAudioButton.vue
@@ -1,14 +1,32 @@
 <script setup lang="ts">
-    // import { ref, } from 'vue'
+    import { ref, } from 'vue'
+
+    const active = ref(false)
+    let audioInterval: NodeJS.Timeout | null;
+
+    function toggle() {
+        console.log(new Date(Date.now()).toISOString())
+        active.value = !active.value;
+        if (active.value) {
+            playAudio();
+            audioInterval = setInterval(playAudio, 200);
+        } else {
+            if (audioInterval !== null) {
+                clearInterval(audioInterval);
+                audioInterval = null;
+            }
+        }
+    }
 
     async function playAudio(){
+        console.log(new Date(Date.now()).toISOString())
         const audio = new Audio('/LegoweltSnare001.wav')
         audio.play()
     }
 </script>
 
 <template>
-    <button @click="playAudio">play audio</button>
+    <button @click="toggle">{{ active ? "stop" : "play"}}</button>
 </template>
 
 <style scoped>
